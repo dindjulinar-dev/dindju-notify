c965892e1a8fb4313484799161ab0ba13589944d
dindjulinar-dev <mathisdzme@outlook.fr>
2026-06-03 15:14:03 +0200

    Add files via upload

---
+++ Dindju-Notify-5/dindju notify/LockedForm.vb
@@ -0,0 +1,13 @@
+﻿Public Class LockedForm
+
+    Private Sub LockedForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.FormBorderStyle = FormBorderStyle.None
+        Me.WindowState = FormWindowState.Maximized
+        Me.TopMost = True
+
+    End Sub
+
+    Protected Overrides Sub OnFormClosing(e As FormClosingEventArgs)
+        e.Cancel = True
+    End Sub
+End Class
---
+++ Dindju-Notify-5/dindju notify/MAJs.vb
@@ -0,0 +1,37 @@
+﻿Imports System.Net
+Imports System.Text.RegularExpressions
+
+Public Class MAJs
+    Private Sub MAJs_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim currentVersion As New Version("4.0.3")
+        Dim latestVersion As Version = GetLatestGitHubReleaseVersion()
+
+        If latestVersion IsNot Nothing AndAlso latestVersion > currentVersion Then
+            MessageBox.Show("Une nouvelle version est disponible : " & latestVersion.ToString() & " | Version actuelle : " & currentVersion.ToString, "Mise à jour disponible", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+        Me.Close()
+    End Sub
+
+    Private Function GetLatestGitHubReleaseVersion() As Version
+        Try
+            ' Forcer TLS 1.2 pour GitHub
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12
+
+            Dim client As New WebClient()
+            client.Headers.Add("User-Agent", "Mozilla/5.0")
+            Dim html As String = client.DownloadString("https://github.com/DINDJU/Dindju-Notify/releases")
+
+            Dim match As Match = Regex.Match(html, "releases/tag/v?(\d+\.\d+\.\d+)", RegexOptions.IgnoreCase)
+            If match.Success Then
+                Return New Version(match.Groups(1).Value)
+            End If
+        Catch ex As Exception
+            MessageBox.Show("Erreur lors de la vérification des mises à jour : " & ex.Message, "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+
+        End Try
+
+        Return Nothing
+
+    End Function
+
+End Class
