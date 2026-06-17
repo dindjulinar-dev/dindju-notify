7e45dd19ba034873d93980578e7b7543a6714dd0
dindjulinar-dev <mathisdzme@outlook.fr>
2026-06-17 13:19:42 +0200

    Add files via upload

---
+++ dindju-notify-5.1.0/Dindju-Notify-5/dindju notify/LockedForm.Designer.vb
@@ -0,0 +1,58 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class LockedForm
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(LockedForm))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(743, 486)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'LockedForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(743, 489)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "LockedForm"
+        Me.Text = "LockedForm"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ dindju-notify-5.1.0/Dindju-Notify-5/dindju notify/Scanneur_d_ip.Designer.vb
@@ -0,0 +1,104 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Scanneur_d_ip
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Scanneur_d_ip))
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.txtBaseIP = New System.Windows.Forms.TextBox()
+        Me.btnScan = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.Font = New System.Drawing.Font("Comic Sans MS", 9.75!)
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 18
+        Me.ListBox1.Location = New System.Drawing.Point(5, 87)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(331, 310)
+        Me.ListBox1.TabIndex = 0
+        '
+        'txtBaseIP
+        '
+        Me.txtBaseIP.Font = New System.Drawing.Font("Comic Sans MS", 11.25!)
+        Me.txtBaseIP.Location = New System.Drawing.Point(5, 24)
+        Me.txtBaseIP.Name = "txtBaseIP"
+        Me.txtBaseIP.Size = New System.Drawing.Size(229, 28)
+        Me.txtBaseIP.TabIndex = 1
+        Me.txtBaseIP.Text = "192.168.1.0"
+        '
+        'btnScan
+        '
+        Me.btnScan.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnScan.Location = New System.Drawing.Point(240, 24)
+        Me.btnScan.Name = "btnScan"
+        Me.btnScan.Size = New System.Drawing.Size(96, 28)
+        Me.btnScan.TabIndex = 2
+        Me.btnScan.Text = "Scan"
+        Me.btnScan.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(5, 58)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(331, 23)
+        Me.ProgressBar1.TabIndex = 3
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Comic Sans MS", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(3, 3)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(140, 18)
+        Me.Label4.TabIndex = 12
+        Me.Label4.Text = "Sous réseau de base :"
+        '
+        'Scanneur_d_ip
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(340, 402)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.btnScan)
+        Me.Controls.Add(Me.txtBaseIP)
+        Me.Controls.Add(Me.ListBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Scanneur_d_ip"
+        Me.Text = "Scanneur d'ip Dindju Notify"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents txtBaseIP As TextBox
+    Friend WithEvents btnScan As Button
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Label4 As Label
+End Class
